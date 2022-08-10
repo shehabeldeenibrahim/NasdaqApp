e2f67f3db49118c4629cd3fb9460d4bc1af42221
Shehab Ibrahim <shehabtibrahim@gmail.com>
2022-08-10 15:13:55 +0200

    feat: add theme config

---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { CreateThemeOptions } from "@rneui/themed";
+
+export const theme: CreateThemeOptions = {
+  lightColors: {
+    primary: "#2785d6",
+    secondary: "#FFFFFF",
+    background: "#eaf2fb",
+  },
+  darkColors: {
+    primary: "black",
+    secondary: "black",
+  },
+  mode: "light",
+};
