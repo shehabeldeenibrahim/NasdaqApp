7a9a39eb1047382cbd1b1fce7fb69337fbaecb69
Shehab Ibrahim <shehabtibrahim@gmail.com>
2022-08-13 13:36:11 +0200

    feat: add search logic to tickers list

--- src/Screens/TickersScreen.tsx
+++ src/Screens/TickersScreen.tsx
@@ -1,5 +1,5 @@
 import { SearchBar } from "@rneui/base";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import TickerList from "../Components/TickersList/TickersList";
 import { TickersData } from "../mocks/Tickers";
@@ -15,10 +15,14 @@ interface Props {
 const TickersScreen: React.FC<Props> = ({ navigation }) => {
   const { searchTickers, retrieveMoreTickers } = useActions();
   const { tickers } = useAppState();
+  const [query, setQuery] = useState("");
 
+  const handleChange = (query: string) => {
+    setQuery(query);
+  };
   useEffect(() => {
-    searchTickers("");
-  }, []);
+    searchTickers(query);
+  }, [query]);
   return (
     <View>
       <SearchBar
@@ -29,6 +33,8 @@ const TickersScreen: React.FC<Props> = ({ navigation }) => {
         }}
         inputContainerStyle={{ backgroundColor: "white" }}
         leftIconContainerStyle={{ backgroundColor: "white" }}
+        onChangeText={handleChange}
+        value={query}
       />
       <TickerList data={tickers} retrieveMore={retrieveMoreTickers} />
     </View>
