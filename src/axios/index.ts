b74ae8a2b8bdda5f784813d23eda4f2f7d0b9823
Shehab Ibrahim <shehabtibrahim@gmail.com>
2022-08-13 13:24:36 +0200

    feat: add axios interceptor

---
+++ src/axios/index.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { APIKEY } from "../Constants";
+
+const customAxios = axios.create({
+  baseURL: "https://api.polygon.io",
+});
+customAxios.defaults.params = {};
+customAxios.interceptors.request.use(
+  function (config) {
+    config.params["apikey"] = APIKEY;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+export default customAxios;
