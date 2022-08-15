52d52651a44f4eefbcb9093c145503d939b9ff4b
Shehab Ibrahim <shehabtibrahim@gmail.com>
2022-08-15 16:42:05 +0200

    test: add unit test for TickersList test onEndReach and renders

--- src/mocks/Tickers.ts
+++ src/mocks/Tickers.ts
@@ -4,74 +4,47 @@ export const TickersData: Ticker[] = [
   {
     ticker: "AAPL",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "stocks",
   },
   {
     ticker: "NIKE",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "crypto",
   },
   {
     ticker: "NETFLIX",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "fx",
-  },
-  {
-    ticker: "MACC",
-    name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
-    market: "otc",
   },
   {
     ticker: "AAPL",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "stocks",
   },
   {
     ticker: "NIKE",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "crypto",
   },
   {
     ticker: "NETFLIX",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "fx",
-  },
-  {
-    ticker: "MACC",
-    name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
-    market: "otc",
   },
   {
     ticker: "AAPL",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "stocks",
   },
   {
     ticker: "NIKE",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "crypto",
   },
   {
     ticker: "NETFLIX",
     name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
     market: "fx",
-  },
-  {
-    ticker: "MACC",
-    name: "Mission Advancement Corp.",
-    logo: "https://i.pravatar.cc/300",
-    market: "otc",
   },
 ];
 export const stats: Stats = {
@@ -90,6 +63,8 @@ export const TickerDetailsData: TickerDetails = {
   website: "http://www.google.com",
   currency: "USD",
   industry: "industry",
+  historicalPrices: null,
+  percentageChange: 0,
   description:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dolor est, tempor vitae rhoncus in, tincidunt sagittis nisi. Vivamus sit amet diam quis libero mollis suscipit. Aliquam mattis nec purus in convallis. Quisque id lorem vitae nibh malesuada dignissim at at turpis. Duis mattis semper consequat. Donec urna arcu, facilisis vel rutrum vel, imperdiet quis urna. Etiam ornare erat vitae dictum sollicitudin. Donec sed ex sed nisi hendrerit rutrum.Nullam vel erat ut velit interdum efficitur. Suspendisse id porta velit. Praesent ultricies pulvinar dictum. Morbi lacus leo, facilisis eget libero nec, ultricies dignissim ante. Aliquam pretium turpis gravida tellus tristique, ac dictum magna interdum. Aliquam viverra, tellus eu congue vestibulum, metus turpis vehicula lacus, ac aliquam odio eros sit amet lacus. Quisque et tincidunt velit.Sed efficitur elementum dolor, non sagittis dolor tempor at. Quisque orci tortor, sollicitudin eu faucibus nec, vehicula at enim. Praesent eget metus ultricies, eleifend metus at, faucibus massa. Donec quis quam sollicitudin, congue ligula non, tincidunt sem. Maecenas ut mi vel ligula tincidunt dapibus. Donec facilisis sodales lacus et vestibulum. Nam eu tristique sapien. Fusce egestas hendrerit nulla, at malesuada quam blandit quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque commodo sapien, sed accumsan libero aliquam in. Proin pharetra dui vel neque fermentum, quis cursus lectus semper.Nulla ultrices enim consectetur posuere semper. Nunc a fringilla velit. Morbi iaculis, lacus vitae interdum commodo, magna lorem varius felis, ac aliquet orci justo nec urna. Mauris varius vestibulum lacus, ut accumsan tellus tincidunt pulvinar. Sed luctus tempor neque, at suscipit leo finibus vitae. Donec et hendrerit dui, et vehicula dolor. Maecenas euismod ex sed semper eleifend. Praesent elementum neque vel volutpat dapibus.",
 };
