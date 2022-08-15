c659efdc35642481457735abfb48b18c1993ff37
Shehab Ibrahim <shehabtibrahim@gmail.com>
2022-08-15 18:11:09 +0200

    test: add tests for InfoTab

---
+++ src/Components/TickerDetails/__tests__/InfoTab.test.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { render, screen } from "@testing-library/react-native";
+import InfoTab from "../InfoTab";
+import { TickerDetailsData } from "../../../mocks/Tickers";
+
+// Mock Navigation
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      dispatch: jest.fn(),
+    }),
+  };
+});
+// Mock Icon
+jest.mock("@fortawesome/react-native-fontawesome", () => ({
+  FontAwesomeIcon: "",
+}));
+beforeEach(() => {
+  render(
+    <InfoTab
+      description={TickerDetailsData.description}
+      website={TickerDetailsData.website}
+    />
+  );
+});
+
+describe("Info Tab", () => {
+  test("Render title and description", () => {
+    // Check for correct description rendered
+    expect(
+      screen.getAllByText(
+        TickerDetailsData.description ? TickerDetailsData?.description : ""
+      )
+    ).toBeDefined();
+    // Check for correct title rendered
+    expect(screen.getByText("About")).toBeDefined();
+  });
+
+  test("Render website button", () => {
+    // Check for correct title rendered
+    expect(screen.getByText("Visit Website")).toBeDefined();
+  });
+
+  test("Render website button if null passed", () => {
+    let newDetails = Object.assign({}, TickerDetailsData);
+    render(<InfoTab description={newDetails.description} website={null} />);
+    // Check for correct title rendered
+    expect(screen.queryByText("Visit Website")).toBeNull();
+  });
+});
